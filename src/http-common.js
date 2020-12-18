48710ed96f5a81754d1c805a799ee5bd4b6b2bd8
bob <jainaman091996@gmail.com>
2020-12-18 17:34:20 +0530

    add

--- src/http-common.js
+++ src/http-common.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://law-b.herokuapp.com/api",
+  baseURL: "https://law-b.herokuapp.com/api",
    headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
-            
+
             }
  
 });
